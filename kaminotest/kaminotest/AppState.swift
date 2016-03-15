8e47d5f3b57ca1241c9bf65510745927cd338c56
amela <amela.rakanovic@gmail.com>
2016-03-15 10:46:09 +0100

    Pressing left, right buttons should move trough months. (3.2)

--- kaminotest/kaminotest/AppState.swift
+++ kaminotest/kaminotest/AppState.swift
@@ -9,6 +9,7 @@
 import Foundation
 
 class AppState {
+    
     static let sharedstate = AppState()
     
     let date = NSDate()
@@ -17,6 +18,37 @@ class AppState {
     
     var monthStateString: String?
     
-        
+    func addMonthToDate () -> NSDate? {
+        if let stateDate = self.stateDate {
+            let date = NSCalendar.currentCalendar().dateByAddingUnit(.Month, value: 1, toDate: stateDate, options: [])
+            return date!
+        }
+        return nil
+    }
+    
+    func substractMonthToDate () -> NSDate? {
+        if let stateDate = self.stateDate {
+            let date = NSCalendar.currentCalendar().dateByAddingUnit(.Month, value: -1, toDate: stateDate, options: [])
+            return date!
+        }
+        return nil
+    }
+    
+    func getMonthString () -> String {
+        let formatter = NSDateFormatter()
+        formatter.timeStyle = .ShortStyle
+        formatter.dateFormat = "MMMM"
+        AppState.sharedstate.monthStateString = formatter.stringFromDate(date)
+        let monthString = formatter.stringFromDate(AppState.sharedstate.stateDate!)
+        return (monthString)
+    }
+    
+    
+    func formateDateToMonthString() {
+        let formatter = NSDateFormatter()
+        formatter.timeStyle = .ShortStyle
+        formatter.dateFormat = "MMMM"
+        AppState.sharedstate.monthStateString = formatter.stringFromDate(AppState.sharedstate.stateDate!)
 
+    }
 }
--- kaminotest/kaminotest/ViewController.swift
+++ kaminotest/kaminotest/ViewController.swift
@@ -10,26 +10,54 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    
+    // MARK: -  Outlets
     
     @IBOutlet weak var stateMonthLabel: UILabel!
+    
+    
+    // MARK: -  Button actions
+    
+    @IBAction func leftButton(sender: UIButton) {
+        print("month--")
+        AppState.sharedstate.stateDate = AppState.sharedstate.substractMonthToDate()
+        
+        AppState.sharedstate.formateDateToMonthString()
+        
+        updateLabel()
+    }
+    
+    
+    @IBAction func rightButton(sender: UIButton) {
+        print("month++")
+        AppState.sharedstate.stateDate = AppState.sharedstate.addMonthToDate()
+        
+        AppState.sharedstate.formateDateToMonthString()
+        
+        updateLabel()
+
+        
+    }
+    
+    // MARK: -  Functions
+    
+    func updateLabel() {
+        if let month = AppState.sharedstate.monthStateString {
+            //print(month)
+            stateMonthLabel.text = month
+        }
+    
+    }
+    
+    // MARK: -  Override
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
         AppState.sharedstate.stateDate = AppState.sharedstate.date
-        print(AppState.sharedstate.stateDate)
+        //print(AppState.sharedstate.stateDate)
         
-        let formatter = NSDateFormatter()
-        formatter.timeStyle = .ShortStyle
-        formatter.dateFormat = "MMMM"
-        AppState.sharedstate.monthStateString = formatter.stringFromDate(AppState.sharedstate.stateDate!)
-        
-        
-        if let month = AppState.sharedstate.monthStateString {
-            print(month)
-            stateMonthLabel.text = month
-        }
+        AppState.sharedstate.formateDateToMonthString()
+        self.updateLabel()
         
         // Do any additional setup after loading the view, typically from a nib.
     }
